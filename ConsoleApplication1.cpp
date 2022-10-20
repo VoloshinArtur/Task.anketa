41b3e9e0c2fe1edd98dea82316f5f251747763da
VoloshinArtur <115408461+VoloshinArtur@users.noreply.github.com>
2022-10-20 20:08:24 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+#include <string>
+int main()
+{
+	string n, a, h, v;
+	cout << "Input your name";
+	cin >> n;
+	cout << "Input your age";
+	cin >> a;
+	cout << "Input your hight";
+	cin >> h;
+	cout << "Input your ves";
+	cin >> v;
+	cout << "Your profile" << '\n';
+	cout << "Your name:" << n << '\n';
+	cout << "Your age:" << a << '\n';
+	cout << "Your hight:" << h << '\n';
+	cout << "Your weight:" << v << '\n';
+}
+
